9e4ac23b8996780e8ec5c6c8f89eeba126f27ca2
JamesMusembi <james.musembi@student.moringaschool.com>
2022-04-19 12:16:59 +0300

    created github-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubAppComponent } from './components/github-app/github-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubAppComponent,
+  
   ],
   imports: [
     BrowserModule,
